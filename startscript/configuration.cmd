c1ee11084de66e6f8027fafeecfdbbcfa154f1d1
Main Fighter <MainFighter@users.noreply.github.com>
2018-06-24 08:01:34 +1000

    autodownload/update and other improvements

--- startscript/configuration.cmd
+++ startscript/configuration.cmd
@@ -1,10 +1,23 @@
 :: Global Configuration
 :: set killrunning=true not functional
 :: set phpdownload=true not functional
-:: set botdownload=true not functional
+
+:: Automatically downloads bot files using git
+:: You need git for this to work https://git-scm.com/downloads
+set autodownloadbot=true
+
+:: Automatically updates bot files using git
+:: You need git for this to work https://git-scm.com/downloads
+set autoupdatebot=true
 
 :: Path to PHP Executable
-set phppath=.\php\php.exe
+set phpdir=.\bin\php
+set phppath=%phpdir%\php.exe
 
 :: Path to actual cheat.php file
-set cheatpath=.\bot\cheat.php
+set botdir=.\botfiles
+set botpath=%botdir%\cheat.php
+
+:: Debug Mode
+:: You won't need to use it
+set debug==false
--- startscript/win_startscript.cmd
+++ startscript/win_startscript.cmd
@@ -2,7 +2,8 @@
 
 :: Made by Main Fighter [mainfighter.com]
 :: Simple start script for SteamDB Cheat [https://github.com/SteamDatabase/SalienCheat]
-:: v1.0.0 [23-06-2018]
+:: Adapted from my https://github.com/MainFighter/salien-bot start script for use with this
+:: v1.1.0 [23-06-2018]
 
 ::===============================================================================================================::
 
@@ -15,8 +16,25 @@ echo Starting Sailen Bots
 
 ::===============================================================================================================::
 
+:: Debug
+if %debug%==true @echo on
+
+:: Sets rootdir var to the currently directory of script
+set rootdir=%~dp0
+
+:: Clone botfiles
+echo Downloading bot files
+if %autodownloadbot%==true call :DownloadBotFiles
+if %debug%==true pause
+
+:: Update botfiles
+echo Updating bot files
+if %autoupdatebot%==true call :UpdateBotFiles
+if %debug%==true pause
+
 :: Start all bots in config
 for %%a in ("instances\*.cmd") do call "%%a" & call :StartScript
+if %debug%==true pause
 
 ::===============================================================================================================::
 
@@ -28,19 +46,64 @@ exit
 
 ::===============================================================================================================::
 
+:DownloadBotFiles
+
+echo.
+echo Downloading Bot Files
+
+:: Sets the directory back to the root
+cd "%rootdir%"
+
+:: Checks if bot files don't already exist > if they don't creates folder > if they don't clones bot to directory
+if not exist %botdir% ( mkdir %botdir% & git clone --quiet https://github.com/SteamDatabase/SalienCheat.git %botdir% & echo Bot files downloaded ) else ( echo Bot files already exist )
+
+call :SetDefaults
+
+goto :eof
+
+::===============================================================================================================::
+
+:UpdateBotFiles
+
+echo.
+echo Updating Bot Files
+
+:: Sets the directory back to the root
+cd "%rootdir%"
+
+if exist %botdir% ( cd %botdir% & git pull --quiet & echo Bot files updated ) else ( echo Bot files don't exist )
+
+call :SetDefaults
+
+goto :eof
+
+::===============================================================================================================::
+
 :StartScript
 
-echo Starting %name%
+echo.
+echo %name% - Starting bot
+
+:: Sets the directory back to the root
+cd "%rootdir%"
 
 :: Opens CMD Window > Sets title and color of window > Changes to dir > runs npm install if enabled > starts bot
-set commandline="title Sailen Bot - %name% & color %color% & %phppath% %cheatpath% %token% & exit"
+set commandline="title Sailen Bot - %name% & color %color% & %phppath% %botpath% %token% & exit"
 if %enabled%==true if %minimized%==true (start /min cmd /k  %commandline%) else (start cmd /k %commandline%)
 
+call :SetDefaults
+
+goto :eof
+
+::===============================================================================================================::
+
+:SetDefaults
+
+:: Don't change these
 set token=notconfigured
 set enabled=false
 set minimized=false
 set name=untitled
-set directory=notconfigured
 set color=0C
 
-goto eof
+goto :eof
